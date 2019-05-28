2a3d2c9ce4e511883b4d9c75c6bcb22bac0a1e8f
Jannick Holm <jannick@wonderfestiwall.dk>
2019-05-28 08:59:23 +0200

    -

---
+++ script.js
@@ -0,0 +1,53 @@
+$(function () {
+    var header = $(".desktop-nav");
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scroll");
+        } else {
+            header.removeClass("scroll");
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", start);
+let forside = {};
+
+
+function start() {
+
+
+    function visForsideIndhold() {
+        forside.forEach(forside => {
+            document.querySelector("video").src = forside.baggrunds_video.guid;
+            document.querySelector(".header-content").innerHTML = forside.velkommen_tekst;
+            document.querySelector("#info-container .content p").innerHTML = forside.intro_tekst;
+            document.querySelector("#kurser-container").style.backgroundImage = `url('${forside.kurser_billeder.guid}')`;
+            document.querySelector("#kurser-container .content .top").innerHTML = forside.kurser_teskt;
+            document.querySelector("#mest-solgte-container .billede1 img").src = forside.mest_solgt_billede_et.guid;
+            document.querySelector("#mest-solgte-container .billede2 img").src = forside.mest_solgte_billede_to.guid;
+            document.querySelector("#mest-solgte-container .billede3 img").src = forside.mest_solgte_billede_tre.guid;
+            document.querySelector("#flakkebjerg-container").style.backgroundImage = `url('${forside.flakkebjerg_billede.guid}')`;
+            document.querySelector("#flakkebjerg-container .content").innerHTML = forside.flakkebjerg_tekst;
+
+        });
+
+    }
+
+    async function getJson() {
+        let url = "https://janhol.dk/kea/keramiker/wordpress/wp-json/wp/v2/forside";
+        let jsonData = await fetch(url);
+        forside = await jsonData.json();
+        visForsideIndhold();
+    }
+
+    getJson();
+
+    window.onscroll = () => {
+        const nav = document.querySelector('#navbar');
+        if (this.scrollY <= 10) nav.className = '';
+        else nav.className = 'scroll';
+    };
+}
