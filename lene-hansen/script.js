6e8c954881b97734fa74528bd460b266aa2a2f0f
Julie Freund-Poulsen <juliefreund-poulsen@laptop-vaq5u3ea.kea.local>
2019-05-28 14:06:22 +0200

    hentet alt backend ind på OM

--- lene-hansen/script.js
+++ lene-hansen/script.js
@@ -11,10 +11,11 @@ $(function () {
     });
 
 });
+
 document.addEventListener("DOMContentLoaded", start);
+let om = {};
 
-function (start){
-
+function start() {
     document.querySelector(".uil-bars").addEventListener("click", visMenu);
 
     function visMenu() {
@@ -24,4 +25,35 @@ function (start){
         document.querySelector(".uil").classList.toggle("uil-multiply");
         document.querySelector(".uil-multiply").addEventListener("click", visMenu);
     }
+
+    function visOmIndhold() {
+        om.forEach(om => {
+//            document.querySelector("").src = om.splash.guid;
+            document.querySelector(".splash-content").innerHTML = om.overskrift;
+            document.querySelector(".om-billede img").src = om.billede_om.guid;
+            document.querySelector(".om-tekst").innerHTML = om.tekst;
+            document.querySelector(".cv-tekst").innerHTML = om.cv;
+            document.querySelector(".cv-billede img").src = om.cv_billede.guid;
+            document.querySelector("#kontakt-container h1").innerHTML = om.kontakt_overskrift;
+            document.querySelector("#kontakt-container p").innerHTML = om.kontakt_tekst;
+            document.querySelector("#kontakt-container p").innerHTML = om.google_kort_tekst;
+
+
+        });
+
+    }
+    async function getJson() {
+        let url = "https://janhol.dk/kea/keramiker/wordpress/wp-json/wp/v2/om_lene_hansen";
+        let jsonData = await fetch(url);
+        om = await jsonData.json();
+        visOmIndhold();
+    }
+
+    getJson();
+
+    window.onscroll = () => {
+        const nav = document.querySelector('#navbar');
+        if (this.scrollY <= 10) nav.className = '';
+        else nav.className = 'scroll';
+    };
 }
